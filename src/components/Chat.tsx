e024f36c2536cf057814522dba78b4690b810c15
Takeru9016 <youthfox046@gmail.com>
2023-02-22 19:14:55 +0530

    Minor changes to some design and clean code

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -6,7 +6,7 @@ import { useCollection } from "react-firebase-hooks/firestore";
 
 import { db } from "../../firebase";
 import Message from "./Message";
-import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
+import Homepage from "./Homepage";
 
 export default function Chat({ chatId }: { chatId: string }) {
   const { data: session } = useSession();
@@ -27,14 +27,7 @@ export default function Chat({ chatId }: { chatId: string }) {
   );
   return (
     <div className="flex-1 overflow-y-auto overflow-hidden">
-      {messages?.empty && (
-        <>
-          <p className="mt-10 text-center text-white">
-            Type a prompt in below to get started!
-          </p>
-          <ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 text-white animate-bounce" />
-        </>
-      )}
+      {messages?.empty && <Homepage />}
 
       {messages?.docs.map((message) => (
         <Message key={message.id} message={message.data()} />
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,4 +6,5 @@ export { default as ChatRow } from "./ChatRow"
 export { default as Chat } from "./Chat"
 export { default as ChatInput } from "./ChatInput"
 export { default as ClientProvider } from './ClientProvider'
-export { default as Message } from './Message'   
+export { default as Message } from './Message'
+export { default as Homepage } from './Homepage'
